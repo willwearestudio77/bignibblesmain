cc14a8d5011291e6268d687024317b59e8d75c34
WILLIAM ELLIS <will@wearestudio77.com>
2024-05-17 14:14:22 +0800

    upgraded carousel

--- components/photo-carousel.tsx
+++ components/photo-carousel.tsx
@@ -4,16 +4,16 @@ export async function PhotoCarousel(instagramArray: any) {
   return (
     <Suspense fallback={<div>Loading...</div>}>
       <div className="carousel carousel-center w-full space-x-4  rounded-box p-4">
+        <script src="https://cdn.lightwidget.com/widgets/lightwidget.js"></script>
+        <iframe
+          src="https://cdn.lightwidget.com/widgets/229d9ba59de2563b9f33bfad853cc505.html"
+          scrolling="no"
+          allowTransparency={true}
+          className="lightwidget-widget"
+        ></iframe>
         {instagramArray.instagramArray.map((item: any, index: number) => (
           <div className="cover carousel-item" key={index}>
             <InstagramImage item={item} />
-            <script src="https://cdn.lightwidget.com/widgets/lightwidget.js"></script>
-            <iframe
-              src="//lightwidget.com/widgets/229d9ba59de2563b9f33bfad853cc505.html"
-              scrolling="no"
-              allowTransparency={true}
-              className="lightwidget-widget"
-            ></iframe>
           </div>
         ))}
       </div>
