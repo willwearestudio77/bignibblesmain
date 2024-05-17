7b7e0faae156c8ded5c8540f24fca576915aca74
WILLIAM ELLIS <will@wearestudio77.com>
2024-05-17 14:21:59 +0800

    removed instagram api request

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Footer from 'components/layout/footer';
 import { PhotoCarousel } from 'components/photo-carousel';
 import PartnerUp from 'components/sections/partner-up';
 import Stamps from 'components/stamps';
-import getInstagramData from 'lib/instagram';
+
 import { getCollectionProducts, getHome } from 'lib/shopify';
 import Image from 'next/image';
 import Link from 'next/link';
@@ -23,7 +23,7 @@ export const metadata = {
 
 export default async function HomePage() {
   const data = await getHome();
-  const instagramArray = await getInstagramData();
+
   const products = await getCollectionProducts({
     collection: 'frontpage',
     reverse: true,
@@ -238,7 +238,7 @@ export default async function HomePage() {
           <div className="instagram-section relative bg-accent80 pb-40">
             <h2 className="py-10 text-center text-4xl font-bold uppercase">content</h2>
 
-            <PhotoCarousel instagramArray={instagramArray} />
+            <PhotoCarousel />
             <div className="wave absolute -bottom-1 left-0 w-full  overflow-hidden">
               <svg
                 className="relative block h-[50px] w-full md:h-[116px]"
